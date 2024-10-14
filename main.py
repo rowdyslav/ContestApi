8e10adfc8dd79006643a14dfadae6f33c7bc1350
ZAXph <152623389+ZAXph@users.noreply.github.com>
2024-10-14 23:20:46 +0300

    pep 8

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from routers import contests_router, students_router, projects_router
 
 app = FastAPI(lifespan=db_lifespan)
 
-for router in contests_router, projects_router ,students_router:
+for router in contests_router, projects_router, students_router:
     app.include_router(router)
