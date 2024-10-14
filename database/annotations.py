ceb21c9bb42a9e0a1d4284ec04029db5d8feed63
rowdyslav <rowdyslav@gmail.com>
2024-10-14 22:13:11 +0300

    typing

--- database/annotations.py
+++ database/annotations.py
@@ -4,4 +4,4 @@ from fastapi import File
 from pydantic import BeforeValidator
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
-Picture = Annotated[bytes, BeforeValidator(File)]
+Picture = Annotated[bytes, File]
